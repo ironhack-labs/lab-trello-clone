f52fefa4dc3def123309ed00c3193c0b5554e100
pabloonieva <pablo@ironhack.com>
2018-03-18 13:38:09 +0100

    feat: server db config and mongoose install

--- server/app.js
+++ server/app.js
@@ -5,8 +5,10 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// Lesson 1: Require mongoose
-// Lesson 2: Require dotenv configuration
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// Lesson 1: Require mongoose DONE
+// Lesson 2: Require dotenv configuration DONE
 
 const app = express();
 
@@ -22,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mongoose configuration
 // Lesson 1: Mongoose configuration
 // Lesson 2: Use environment variable for the MONGODB_URI
+require('./configs/db.config');
+
 
 app.set('view engine', 'jade');
 
